77d5a14e4384676cb44fdb5500499e105c337454
akaran1729 <akaran@college.harvard.edu>
2023-02-11 21:05:15 -0500

    client side coded except for list accounts

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import socket
 import select
 import sys
 
+MAX_MESSAGE_LENGTH = 280
+MAX_RECIPIENT_LENGTH = 50
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
     print("Correct usage: script, IP address, port number")
@@ -14,29 +17,78 @@ server.connect((IP_address, Port))
 
 
 # Keywords that client side parses and tags to send to the server
-MESSAGE_KEYS = ['Create Account: ', 'Login: ', 'Logout: ']
+MESSAGE_KEYS = ['Create Account: ', 'Login: ', 'Logout', 'Delete Account', 'Send']
 
 
 
 #To Do: limits on message and username lengths
 
 # always returns encoded message
-def append_tag(message):
+def process(message):
     message.rstrip()
-    if message.find('Create Account: ') == 0:
-        message = message[16:]  # remove function instead
+    # Messages are first entered as types, and are tagged based on those types
+    if message.find('Create Account') == 0:
+        name = ""
+        name_max = False
+        # Ensures created accounts are no more than the max alotted length
+        while name_max == False:
+            name = sys.stdin.readline()
+            if len(name) <= MAX_RECIPIENT_LENGTH:
+                name_max = True
+            else:
+                print("All usernames must be at most " + str(MAX_RECIPIENT_LENGTH) + " characters. Please try again.")
+        message = name
         message = message.encode()
         tag = (0).to_bytes(1, "big")
-    elif message.find('Login: ') == 0:
-        message = message[7:]
+    elif message.find('Login') == 0:
+        message = sys.stdin.readline()
         message = message.encode()
         tag = (1).to_bytes(1, "big")
-    elif message.find('Logoff: ') == 0 or message.find('Logout: '):
-        message = message[8:]
+    elif message.find('Logout') == 0:
+        message = ""
         message = message.encode()
         tag = (2).to_bytes(1, "big")
+    elif message.find('Delete Account') == 0:
+        message = ""
+        message = message.encode()
+        tag = (3).to_bytes(1, "big")
+    elif message.find("Send") == 0:
+        print("To: ")
+        recipient = ""
+        rec_max = False
+        # Ensures recipient is in the length limit, for wire protocol tagging
+        while rec_max == False:
+            recipient = sys.stdin.readline()
+            if len(recipient) <= MAX_RECIPIENT_LENGTH:
+                rec_max = True
+            else:
+                print("All usernames are at most " + str(MAX_RECIPIENT_LENGTH) + " characters. Please try again.")
+        len_r = len(recipient)
+        recep_tag = (len_r).to_bytes(1, "big")
+        recipient = recipient.encode()
+        print("Message: ")
+        mes_len = False
+        message = ""
+        # Ensures message is in the length limit
+        while mes_len == False:
+            message = sys.stdin.readline()
+            if len(message) <= MAX_MESSAGE_LENGTH:
+                mes_len = True
+            else:
+                print("Message must be at most " + str(MAX_MESSAGE_LENGTH) + " characters. Please try again.")
+        message = message.encode()
+        type_tag = (4).to_bytes(1, "big")
+        # Wire protocol tags with type, then length of receiving username for metadata parsing, and the receiving username
+        tag = type_tag + recep_tag + recipient
+    elif message.find("Open Undelivered Messages") == 0:
+        message = ""
+        message = message.encode()
+        tag = (5).to_bytes(1, "big")
+    elif message.find("List Accounts") == 0:
+        # To Do
+        pass
     else:
-        print('formatting error')
+        print('Input not recognized. Please try again.')
         return
 
     bmsg = tag + message
@@ -66,11 +118,18 @@ while True:
             print(dmessage)
         else:
             message = sys.stdin.readline()
-            bmsg = append_tag(message)
-            if bmsg and bmsg[0] != 4:
+            bmsg = process(message)
+            # if bmsg and bmsg[0] != 4: ??? To Do
+            if bmsg:
                 try: 
                     server.send(bmsg)
                     sys.stdout.flush()
                 except: 
                     print('message could not send')
 server.close()
+
+
+
+# To Do: If we log in, and try to log in again, do we have to have a client side error, or can we just let that be a null operation (second option probably good too)
+# To Do: Probably more comments about the wire protocol
+# To Do: Big Endian Handling
